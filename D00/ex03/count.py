e3fbbb8b55d9830a138ce22ef46743b318db58fb
qbarrier <qbarrier@gmail.com>
2020-05-26 16:36:28 +0200

    ex03

---
+++ D00/ex03/count.py
@@ -0,0 +1,14 @@
+def text_analyzer(*texte):
+    punct = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+    if len(texte) > 1:
+        print("ERROR")
+        quit()
+    elif len(texte) == 0:
+        texte = str(input("What is the text to analyse?"))
+    else:
+        texte = str(texte[0])
+    print("The text contains ", len(texte) ," characters:\n\n")
+    print("- " , sum(1 for letter in texte if letter.isupper()) , " upper letters\n\n")
+    print("- " , sum(1 for letter in texte if letter.islower()) , " lower letters\n\n")
+    print("- " , sum(1 for letter in texte if letter in punct), " punctuation marks\n\n")
+    print("- " , sum(1 for letter in texte if letter.isspace()) , " spaces")
